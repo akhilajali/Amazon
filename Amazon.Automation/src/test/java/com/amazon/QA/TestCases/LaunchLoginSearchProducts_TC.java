6e3f161d53672d7456f476122b597dfc04b45537
vanithau <vanithau16@gmail.com>
2023-02-16 14:23:16 +0530

    This is second commit

--- Amazon.Automation/src/test/java/com/amazon/QA/TestCases/LaunchLoginSearchProducts_TC.java
+++ Amazon.Automation/src/test/java/com/amazon/QA/TestCases/LaunchLoginSearchProducts_TC.java
@@ -36,6 +36,10 @@ public class LaunchLoginSearchProducts_TC  extends BaseClass
 	{
 		llpo.LoginPage();
 	}
+	public void display()
+	{
+		System.out.println("This is first commit by vanitha");
+	}
 }
 
 
